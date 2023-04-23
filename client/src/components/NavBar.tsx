9cd22f2a69e62b75614a8201c9b7d5260069a7af
oggy107 <urmalsingh10@gmail.com>
2023-04-23 14:35:50 +0530

    fixed: drawer issues on small devices

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect, useCallback } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import { CustomButton } from "./";
@@ -47,6 +47,19 @@ const NavBar = () => {
     const { setActive, active } = useContext(navContext);
     const [toggleDrawer, setToggleDrawser] = useState(false);
 
+    // close drawer on scroll
+    const handleScroll = useCallback(() => {
+        setToggleDrawser(false);
+    }, []);
+
+    useEffect(() => {
+        if (toggleDrawer) {
+            document.addEventListener("scroll", handleScroll);
+        } else {
+            document.removeEventListener("scroll", handleScroll);
+        }
+    }, [toggleDrawer]);
+
     return (
         <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
             <div className="lg:flex-1 flex flex-row md:max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-full">
@@ -84,10 +97,21 @@ const NavBar = () => {
 
             {/* small screen navigation */}
             <div className="sm:hidden flex justify-between items-center relative">
+                {/* overlay to close drawer on clikcing somewhere else */}
+                <div
+                    className={`absolute w-full h-screen top-[50px] z-10 ${
+                        toggleDrawer == false && "hidden"
+                    }`}
+                    onClick={() => {
+                        setToggleDrawser(false);
+                    }}
+                ></div>
                 <div
                     className="w-[40px] h-[40px] flex justify-center items-center cursor-pointer bg-[#2c2f32] rounded-[10px]"
                     onClick={() => {
+                        setToggleDrawser(false);
                         navigate("/");
+                        setActive!(NavLinkNames.DASHBOARD);
                     }}
                 >
                     <img
